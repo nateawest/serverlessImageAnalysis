0a2ee410f868ac2b2bfa36c8739ab33255dbfb17
Nathan west <104093859+nateawest@users.noreply.github.com>
2024-04-23 22:45:17 +0000

    added .env and load_dotenv

--- lambda.py
+++ lambda.py
@@ -1,9 +1,20 @@
 import boto3
+import os
 
-# Create an S3 service client
-s3 = boto3.client('s3')
+from dotenv import load_dotenv
 
-# List existing buckets
-buckets = s3.list_buckets()
-for bucket in buckets['Buckets']:
-    print(bucket['Name'])
+def test_sdk():
+    # Create an S3 service client
+    s3 = boto3.client('s3')
+
+    # List existing buckets
+    buckets = s3.list_buckets()
+    for bucket in buckets['Buckets']:
+        print(bucket['Name'])
+
+
+load_dotenv()  # load environment variables from .env file
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+test_sdk()
