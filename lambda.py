7c08455da20ddaae33416cc39138caf5459aaa72
Nathan west <104093859+nateawest@users.noreply.github.com>
2024-04-23 20:33:30 +0000

    Testing boto3 python SDK for AWS

--- lambda.py
+++ lambda.py
@@ -1,1 +1,9 @@
+import boto3
 
+# Create an S3 service client
+s3 = boto3.client('s3')
+
+# List existing buckets
+buckets = s3.list_buckets()
+for bucket in buckets['Buckets']:
+    print(bucket['Name'])
