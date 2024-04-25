515fe28752d3490daf930389f1ca821decd9e88d
Nathan west <104093859+nateawest@users.noreply.github.com>
2024-04-25 02:24:50 +0000

    Implement Dynamodb

--- lambda.py
+++ lambda.py
@@ -1,13 +1,14 @@
 import boto3
+from decimal import Decimal
+from dotenv import load_dotenv
 import os
 
-from dotenv import load_dotenv
-
 def test_sdk():
-    # Create an S3 service client
+    # testing out boto3 sdk with simple list buckets function
+    # S3 service client
     s3 = boto3.client('s3')
 
-    # List existing buckets
+    # List all buckets
     buckets = s3.list_buckets()
     for bucket in buckets['Buckets']:
         print(bucket['Name'])
@@ -17,13 +18,26 @@ def rekognition_test():
         Image={
             'S3Object': {
                 'Bucket': 'bucketsoc',
-                'Name': 'pic1.JPG'
+                'Name': 'IMG_0004.JPG'
             }
         },
+        # Number of items rekognition will identify in image.
         MaxLabels = 3,
+        # Confidence rekognition has in the identified label
         MinConfidence = 75
     )
 
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+    table = dynamodb.Table('ImageTags')
+
+    # process image, had to use decimal library to convert float to int because dynamodb does not store floats. Alt could store float as a string
+    labels = [{'Label': label['Name'], 'Confidence': Decimal(str(round(label['Confidence'], 2)))} for label in response['Labels']]
+    table.put_item(
+        Item={
+            'image_id': 'IMG_0004.JPG',
+            'labels': labels
+        }
+    )
 
 load_dotenv()  # load environment variables from .env file
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
@@ -31,6 +45,7 @@ AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
 test_sdk()
 
+# must be same region as S3 bucket
 rekognition_client = boto3.client('rekognition', region_name='us-east-2')
 rekognition_test()
 
